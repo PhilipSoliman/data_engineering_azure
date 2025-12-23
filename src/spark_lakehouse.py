64939e80598703af4056133292986fe0b57f91dd
PhilipSoliman <philip.soliman@digital-power.com>
2025-12-23 11:20:43 +0100

    Updated and installed connect variant of pyspark

--- src/spark_lakehouse.py
+++ src/spark_lakehouse.py
@@ -1,15 +1,21 @@
 # import SparkSession
 from pyspark.sql import SparkSession
 
+
 # Connect to the Dockerized Spark standalone master (port 7077 per docker-compose)
 def get_spark_session(app_name: str) -> SparkSession:
     spark = (
-        SparkSession.builder
-        .master("spark://localhost:7077") # type: ignore
+        SparkSession.builder.remote("sc://localhost:15002")  # type: ignore
         .appName(app_name)
-        .config("spark.driver.host", "localhost")
+        # # When running the Python driver on Windows and Spark workers in Docker,
+        # # advertise the host machine so executors can connect back to the driver.
+        # # Docker Desktop exposes the host as `host.docker.internal` on Linux containers.
+        # .config("spark.driver.host", "host.docker.internal")
+        # # Bind address for the driver (allow container connections)
+        # .config("spark.driver.bindAddress", "0.0.0.0")
+        # # Fix the driver port so containers know which port to connect to
+        # .config("spark.driver.port", "49992")
         .getOrCreate()
     )
-    print("Connected to Spark master:", spark.sparkContext.master)
-    print("Spark version:", spark.version)
+    print(f"Connected to Spark app {spark.conf.get('spark.app.name')} (v. {spark.version})")
     return spark
